8ad31e62622402fc6f81cd96009a79b5c8a0a30d
Swdenis <swdenis@mail.ru>
2021-05-11 10:11:56 +0200

    Add songlist

---
+++ client/components/SongList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+class SongList extends React.Component {
+	renderSongs() {
+		return this.props.data.songs.map((song) => {
+			return (
+				<li className="collection-item" key={song.id}>
+					{song.title}
+				</li>
+			)
+		})
+	}
+
+	render() {
+		console.log(this.props.data.songs)
+		if (this.props.data.loading) {
+			return <div>Loading...</div>
+		}
+		return <ul className="collection">{this.renderSongs()}</ul>
+	}
+}
+
+const query = gql`
+	{
+		songs {
+			id
+			title
+		}
+	}
+`
+
+export default graphql(query)(SongList)
