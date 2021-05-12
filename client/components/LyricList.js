7f4d9ef1801315c87ec127aa8d82b0da40d411b5
Swdenis <swdenis@mail.ru>
2021-05-12 18:09:57 +0200

    like lyric

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import {graphql} from 'react-apollo'
+
 
 class LyricList extends React.Component {
+	onLike(id) {
+		console.log(id)
+	}
+
 	renderLyrics() {
 		return this.props.lyrics.map(({ id, content }) => {
 			return (
 				<li key={id} className="collection-item">
 					{content}
+					<i className="material-icons" onClick={() => this.onLike(id)}>
+						thumb_up
+					</i>
 				</li>
 			)
 		})
@@ -16,4 +26,12 @@ class LyricList extends React.Component {
 	}
 }
 
-export default LyricList
+const mutation = gql`
+	mutation likeLyric($id: ID!) {
+		likeLyric(id: $id) {
+		id
+		likes
+		}
+	}
+`
+export default graphql(mutation)(LyricList)
