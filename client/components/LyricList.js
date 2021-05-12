06878b5247d02f1867df3d62333018743fab1aab
Swdenis <swdenis@mail.ru>
2021-05-12 18:34:20 +0200

    wire up optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,8 +3,18 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class LyricList extends React.Component {
-	onLike(id) {
-		this.props.mutate({ variables: { id } })
+	onLike(id, likes) {
+		this.props.mutate({
+			variables: { id },
+			optimisticResponse: {
+				__typename: 'Mutation',
+				likeLyric: {
+					id: id,
+					likes: likes + 1,
+					__typename: 'LyricType'
+				}
+			}
+		})
 	}
 
 	renderLyrics() {
@@ -13,10 +23,12 @@ class LyricList extends React.Component {
 				<li key={id} className="collection-item">
 					{content}
 					<div className="vote-box">
-					<i className="material-icons" onClick={() => this.onLike(id)}>
-						thumb_up
-					</i>
-					{likes}
+						<i
+							className="material-icons"
+							onClick={() => this.onLike(id, likes)}>
+							thumb_up
+						</i>
+						{likes}
 					</div>
 				</li>
 			)
